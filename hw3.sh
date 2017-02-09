484482abed4088edc2705cc9c4af6a2ab533d6c2
Jon <jonathanwheeler8@gmail.com>
2017-02-09 19:14:07 +0000

    added shell script

---
+++ hw3.sh
@@ -0,0 +1,62 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: hw3.sh
+# 
+#         USAGE: ./hw3.sh 
+# 
+#   DESCRIPTION: 
+# 
+#         NOTES: ---
+#        AUTHOR: Jon Wheeler (), jonathanwheeler1@weber.edu
+#       CREATED: 02/09/2017 18:37
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+help()
+{
+	echo "Usage"
+	exit 1;
+}
+
+if [[ $1 == "--help" ]]
+then
+	help
+fi
+
+while getops ":s:a:i:" opt
+do
+	case $opt in
+		s) sedFile=$OPTARG
+			;;
+		a) awkFile=$OPTARG
+			;;
+		i) inFile=$OPTARG
+			;;
+		/?)
+			help
+			;;
+	esac
+done
+
+if [[ -z $sedFile ]]
+then
+	echo "Sed file missing"
+	help
+fi
+
+if [[ -z $awkFile ]]
+then
+	echo "Awk file missing"
+	help
+fi
+
+if [[ -z $inFile ]]
+	echo "inFile is missing"
+	help
+fi
+
+exit 0
+
